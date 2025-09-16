b164cc0bc4c16afb1697abd76bd219f406f2445b
Maanas Nair <63333626+itsmebirdie@users.noreply.github.com>
2025-09-17 04:08:43 +0530

    feat: release registration software

---
+++ sys/YatriSurakshaRegistration/mainwindow.cpp
@@ -0,0 +1,83 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Initialize the network manager
+    networkManager = new QNetworkAccessManager(this);
+
+    // Connect the "Register" button to our slot
+    connect(ui->registerButton, &QPushButton::clicked, this, &MainWindow::on_registerButton_clicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_registerButton_clicked()
+{
+    // Get data from the UI input fields
+    QString govtId = ui->governmentIdInput->text();
+    QString nationality = ui->nationalityInput->text();
+    QString firstName = ui->firstNameInput->text();
+    QString lastName = ui->lastNameInput->text();
+    QString familyContacts = ui->familyContactsInput->toPlainText();
+
+    // Create a JSON object to send to the server
+    QJsonObject touristData;
+    touristData["government_id"] = govtId;
+    touristData["nationality"] = nationality;
+    touristData["first_name"] = firstName;
+    touristData["last_name"] = lastName;
+
+    // Validate and add family contacts
+    QJsonDocument contactsDoc = QJsonDocument::fromJson(familyContacts.toUtf8());
+    if (contactsDoc.isNull() || !contactsDoc.isArray()) {
+        QMessageBox::warning(this, "Input Error", "Family Contacts must be a valid JSON array.");
+        return;
+    }
+    touristData["family_contacts"] = contactsDoc.array();
+
+    // Convert the JSON object to a JSON document
+    QJsonDocument jsonDoc(touristData);
+
+    // Create the HTTP request
+    QNetworkRequest request;
+    request.setUrl(QUrl("http://3.108.41.199:8080/register-tourist"));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+    // Connect the reply signal to our slot
+    connect(networkManager, &QNetworkAccessManager::finished, this, &MainWindow::onRegistrationFinished);
+
+    // Send the POST request
+    ui->statusLabel->setText("Registering...");
+    networkManager->post(request, jsonDoc.toJson());
+}
+
+void MainWindow::onRegistrationFinished(QNetworkReply *reply)
+{
+    // Disconnect the signal to avoid multiple connections
+    disconnect(networkManager, &QNetworkAccessManager::finished, this, &MainWindow::onRegistrationFinished);
+
+    if (reply->error() == QNetworkReply::NoError) {
+        QByteArray responseData = reply->readAll();
+        QJsonDocument jsonResponse = QJsonDocument::fromJson(responseData);
+        QJsonObject jsonObj = jsonResponse.object();
+        QString did = jsonObj["did"].toString();
+
+        ui->statusLabel->setText(QString("Registration Successful! Your DID is:\n %1").arg(did));
+    } else {
+        ui->statusLabel->setText(QString("Registration Failed: %1").arg(reply->errorString()));
+    }
+
+    reply->deleteLater();
+}
